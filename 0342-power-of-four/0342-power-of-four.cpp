52d2ff9fc0ed5a7b3d7ffc74d0a338ad2af8ee30
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2023-10-23 15:55:53 +0530

    Update 0342-power-of-four.cpp

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,3 +1,30 @@
+Approch 1 :
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        int ans=log(n)/log(4);
+        return pow(4,ans)==n;
+    }
+};
+
+Approch 2 :
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        return n==1;
+    }
+};
+
+Approch 3 :
 class Solution {
 public:
     bool isPowerOfFour(int n) {
