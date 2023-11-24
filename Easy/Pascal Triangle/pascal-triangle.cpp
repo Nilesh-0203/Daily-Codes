4f026ed62dc6d5a6e2c6561984ebf1ffbbbdbc10
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2023-11-24 14:20:57 +0530

    Updated Solution - GfG to GitHub

--- Easy/Pascal Triangle/pascal-triangle.cpp
+++ Easy/Pascal Triangle/pascal-triangle.cpp
@@ -8,7 +8,7 @@ class Solution{
 public:
     vector<long long> nthRowOfPascalTriangle(int n) {
         // code here
-       vector<long long> ans(1, 1), t;
+        vector<long long> ans(1, 1), t;
         while (n-- > 1) {
             t.resize(ans.size() + 1, 1);
             for (int i = 1; i < ans.size(); i++) t[i] = (ans[i - 1] + ans[i]) % 1000000007;
