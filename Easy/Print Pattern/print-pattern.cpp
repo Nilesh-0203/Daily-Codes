4872af05008f4b9a0a283acbf965944079a47ed5
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2023-11-26 13:14:35 +0530

    Updated Solution - GfG to GitHub

--- Easy/Print Pattern/print-pattern.cpp
+++ Easy/Print Pattern/print-pattern.cpp
@@ -9,18 +9,14 @@ using namespace std;
 
 class Solution{
 public:
-    vector<int> pattern(int N){
-        // code here
-        int n=N;
-        vector<int>v;
+    void f(int n,vector<int>&v){
         v.push_back(n);
-        while(n>0){
-            n=n-5;
-            v.push_back(n);
-        }
-        for(int i=v.size()-2;i>=0;i--){
-            v.push_back(v[i]);
-        }
+        if(n>0) f(n-5,v);
+        if(n>0) v.push_back(n);
+    }
+    vector<int> pattern(int N){
+        vector<int>v;
+        f(N,v);
         return v;
     }
 };
