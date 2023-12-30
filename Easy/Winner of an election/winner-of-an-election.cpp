f7d7172596f5ca897650793aa047d3bd095372dd
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2023-12-30 12:21:26 +0530

    Updated Solution - GfG to GitHub

--- Easy/Winner of an election/winner-of-an-election.cpp
+++ Easy/Winner of an election/winner-of-an-election.cpp
@@ -13,11 +13,9 @@ class Solution{
   public:
   
     //Function to return the name of candidate that received maximum votes.
-    vector<string> winner(string arr[],int n)
+
+vector<string> winner(string arr[],int n)
     {
-        // Your code here
-        // Return the string containing the name and an integer
-        // representing the number of votes the winning candidate got
         map<string,int>mp;
         for(int i=0;i<n;i++){
             mp[arr[i]]++;
