481b85fe053ec0fc4114fc1f56da1d9a8c9d240b
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-01-05 12:01:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Count possible ways to construct buildings/count-possible-ways-to-construct-buildings.cpp
+++ Medium/Count possible ways to construct buildings/count-possible-ways-to-construct-buildings.cpp
@@ -8,13 +8,14 @@ class Solution{
 	int TotalWays(int N)
 	{
 	    // Code here
+	    long long mod=1e9+7;
 	    long long b=1,s=1,res=2;
 	    for(int i=2;i<=N;i++){
 	        b=s;
 	        s=res;
-	        res=(b+s)%1000000007;
+	        res=(b+s)%mod;
 	    }
-	    return (res*res)%1000000007;
+	    return (res*res)%mod;
 	}
 };
 
