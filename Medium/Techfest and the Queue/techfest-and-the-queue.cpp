8762009a545ae38619f9ec0e5d46a798cb8a9990
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-01-06 12:37:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Techfest and the Queue/techfest-and-the-queue.cpp
+++ Medium/Techfest and the Queue/techfest-and-the-queue.cpp
@@ -7,26 +7,29 @@ using namespace std;
 
 class Solution {
 public:
+
+    int primes(int x){
+        int ans=0;
+        for(int i=2;i*i<=x;i++){
+            while(x%i==0){
+                ans++;
+                x=x/i;
+            }
+        }
+        if(x!=1){
+            ans++;
+        }
+        return ans;
+    }
 	int sumOfPowers(int a, int b){
+	    // Code here
 	    int res=0;
 	    for(int i=a;i<=b;i++){
 	        res+=primes(i);
 	    }
 	    return res;
 	}
-	int primes(int x){
-	        int ans=0;
-	        for(int i=2;i*i<=x;i++){
-	            while(x%i==0){
-	                ans++;
-	                x=x/i;
-	            }
-	        }
-	        if(x!=1){
-	            ans++;
-	        }
-	        return ans;
-	    }
+
 };
 
 //{ Driver Code Starts.
