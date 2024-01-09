7b52327ba57f00636687cc04fc2b187bbe7b42db
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-01-09 16:48:24 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Search Pattern (KMP-Algorithm)/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string txt)
+        {
+            //code here
+            int n=pat.size(),m=txt.size();
+            vector<int>v;
+            for(int i=0;i<m;i++){
+                if(txt[i]==pat[0] and pat==txt.substr(i,n)){
+                    v.push_back(i+1);
+                } 
+            } 
+            return v;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
