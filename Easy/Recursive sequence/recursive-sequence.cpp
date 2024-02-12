48dadfcd1d629df42640f5d3c677e5525fb5f4c2
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-02-12 16:16:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Recursive sequence/recursive-sequence.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long mod=1e9+7;
+    long long sequence(int n){
+        // code here
+        long long ans=1;
+        long long k=1;
+        for(int i=1;i<=n;i++){
+            int count=0;
+            long long a=1;
+            while(count<i){
+                a=(a*k)%mod;
+                k++;
+                count++;
+            }
+            ans=(ans+a)%mod;
+        }
+        return ans-1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.sequence(N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
