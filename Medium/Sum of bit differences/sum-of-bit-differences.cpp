fb2c1fce297111002213dace49f7c48cbedc9ee7
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-02-29 16:17:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of bit differences/sum-of-bit-differences.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	long long sumBitDifferences(int arr[], int n) {
+	    // code here
+	     long cnt = 0;
+        for(int i=31;i>=0;i--){
+           long a = 0;
+           for(int j=0;j<n;j++){
+               a+=(arr[j]>>i)&1;
+           }
+           cnt += 2*a*(n-a);
+        }
+        return cnt;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.sumBitDifferences(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
