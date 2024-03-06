f2b56be7f95f9b10321e26eabfa1b900c025f545
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-03-06 11:55:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.cpp
+++ Medium/Maximum Index/maximum-index.cpp
@@ -10,21 +10,45 @@ class Solution{
     // A[]: input array
     // N: size of array
     // Function to find the maximum index difference.
-    int maxIndexDiff(int a[], int n) 
+     int maxIndexDiff(int a[], int n) 
     { 
         // Your code here
-        int i=0,j=n-1,res =0;
-        while(i<=j){
-            if(a[i]<=a[j]){
-              res=max(res,j-i);
-              j=n-1;
-              i++;
+       
+        int leftMin[n];
+        int rightMax[n];
+        
+        leftMin[0] = a[0];
+        for(int i =1; i<n; i++)
+        {
+            leftMin[i] = min(a[i], leftMin[i-1]);
+        }
+        
+        rightMax[n-1] = a[n-1];
+        for(int j = n-2; j>=0; j--)
+        {
+            rightMax[j] = max(a[j], rightMax[j+1]);
+        }
+        
+        int i =0; 
+        int j=0;
+        int maxDiff  = -1;
+        
+        while(i<n && j<n)
+        {
+            if(leftMin[i] <= rightMax[j])
+            {
+                maxDiff = max(maxDiff, j-i);
+                j = j+1;
             }
-            else{
-              j--;  
-            } 
-        }  
-        return res;
+            
+            else 
+            {
+                i = i +1;
+            }
+        }
+        
+        return maxDiff;
+        
     }
 };
 
