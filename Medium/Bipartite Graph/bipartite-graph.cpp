18d1ca03dbbb7ad48ede45ba003e6ef539d1b38a
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-04-14 12:51:21 +0530

    Updated Solution - GfG to GitHub

--- Medium/Bipartite Graph/bipartite-graph.cpp
+++ Medium/Bipartite Graph/bipartite-graph.cpp
@@ -5,46 +5,38 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-     bool check(int node, int color[], vector<int>adj[])
-    {
-        color[node] = 0;
-        queue<int>q;
-        q.push(node);
-        
-        while(!q.empty())
-        {
-            int n = q.front();
-            q.pop();
-            
-            for(auto it:adj[n])
-            {
-                if(color[it] == -1)
-                {
-                    color[it] = !color[n];
-                    q.push(it);
-                }
-                else if(color[it]==color[n])
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-	bool isBipartite(int V, vector<int>adj[]){
-	    int color[V];
-	    for(int i = 0; i < V; i++) color[i] = -1;
+	bool solve(int node,vector<int>&color,vector<int>adj[]){
+        color[node]=0;
+	    queue<int>q;
+	    q.push(node);
 	    
-	    for(int i = 0; i < V; i++)
-	    {
-	        if(color[i] == -1)
-	        {
-	            if(check(i,color,adj)==false)
+	    while(!q.empty()){
+	        int n=q.front();
+	        q.pop();
+	        
+	        for(auto it:adj[n]){
+	            if(color[it]==-1){
+	                color[it]=!color[n];
+	                q.push(it);
+	            }
+	            else if(color[it]==color[n]){
 	                return false;
+	            }
 	        }
 	    }
 	    return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	     vector<int>color(V,-1);
+	     for(int i=0;i<V;i++){
+	         if(color[i]==-1){
+	             if(solve(i,color,adj)==false){
+	                 return false;
+	             }
+	         }
+	     }
+	     return true;
 	}
 
 };
