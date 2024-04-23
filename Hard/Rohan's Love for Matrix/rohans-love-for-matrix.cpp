eedcf651397546436139fdea83a9108648f176bc
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-04-23 09:36:06 +0530

    Updated Solution - GfG to GitHub

--- Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
+++ Hard/Rohan's Love for Matrix/rohans-love-for-matrix.cpp
@@ -8,17 +8,20 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    int firstElement(int n) {
-        // code here
-        int a=0,b=1;
-        int mod=1e9+7;
-        while(n--){
-            int c=(a+b)%mod;
-            a=b%mod;
-            b=c%mod;
-        }
-        return a;
+    
+
+int firstElement(int n) {
+    int a=0,b=1;
+    int mod=1e9+7;
+    while(n--){
+        int c=(a+b)%mod;
+        a=b%mod;
+        b=c%mod;
+        
     }
+    return a;
+    
+}
 };
 
 //{ Driver Code Starts.
