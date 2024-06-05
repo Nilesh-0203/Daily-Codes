fdc5702721a32e786ff9ab2c5e63b8bdbefeb9a0
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-06-05 17:07:01 +0530

    Updated Solution - GfG to GitHub

--- Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
+++ Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
@@ -9,25 +9,33 @@ class Solution {
   public:
     int findSwapValues(int a[], int n, int b[], int m) {
         // Your code goes here
-        int sum1=0,sum2=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            sum1+=a[i];
-            mp[a[i]]++;
+        int suma = 0;
+        int sumb = 0;
+        
+        for(int i=0; i<n; i++){
+            suma += a[i];
         }
-        for(int i=0;i<m;i++){
-            sum2+=b[i];
+        for(int i=0; i<m; i++){
+            sumb += b[i];
         }
-        int x=abs(sum1-sum2);
-        if(x%2==1){
-            return -1;
+        
+        if((suma - sumb)%2!=0){
+        return -1;
         }
-        if(x==0){
-            return 1;
-        }
-        for(int i=0;i<m;i++){
-            if(mp[(x-b[i])]){
+        int target = (suma-sumb)/2;
+        int i=0, j=0;
+        sort(a, a+n);
+        sort(b, b+m);
+        while(i<n && j<m){
+            int diff = a[i] - b[j];
+            if(diff == target){
                 return 1;
+            }
+            else if(diff<target){
+                i++;
+            }
+            else{
+                j++;
             }
         }
         return -1;
