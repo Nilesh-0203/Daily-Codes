69fa235e4c7e6085024a70b96d91c1b7c4fcfb4e
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-06-16 20:08:05 +0530

    Time: 3 ms (43.62%), Space: 7.5 MB (69.42%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int minAddToMakeValid(string str) {
-        stack<char>st;
-        int cnt=0;
-        for(int i=0;i<str.length();i++){
-            char ch=str[i];
-            if(ch=='('){
-              st.push(ch);
-            }
-            else{
-             if(!st.empty() && st.top()=='('){
-                st.pop();
-             }
-             else{
-                st.push(ch);
-             }
+    int minAddToMakeValid(string p) {
+        int bal = 0;
+        int ans = 0;
+        for (int i = 0; i < p.length(); ++i) {
+            bal += p[i] == '(' ? 1 : -1;
+            // It is guaranteed bal >= -1
+            if (bal == -1) {
+                ans += 1;
+                bal += 1;
             }
         }
-        cnt+=st.size();
-        return cnt;
+        return bal + ans;
     }
 };
