63ea6fca239fcc1f1a41d4cda96a5533877a015d
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-01 19:31:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int solve(vector<vector<int>>& mat,int i,int j,int &maxi,vector<vector<int>>&dp){
+        if(i>=mat.size() || j>=mat[0].size()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int right=solve(mat,i,j+1,maxi,dp);
+        int diagonal=solve(mat,i+1,j+1,maxi,dp);
+        int down=solve(mat,i+1,j,maxi,dp);
+        if(mat[i][j]==1){
+            int ans=1+min(right,min(diagonal,down));
+            maxi=max(maxi,ans);
+            return dp[i][j]= ans;
+        }
+        else{
+            return dp[i][j]= 0;
+        }
+    }
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        // code here
+        int maxi=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        solve(mat,0,0,maxi,dp);
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0;i < n*m;i++)
+            cin>>mat[i/m][i%m];
+        
+        Solution ob;
+        cout<<ob.maxSquare(n, m, mat)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
