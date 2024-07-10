b6bd034d568d8795f06c6dbc9a925c8cc2ef002b
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-10 11:19:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.cpp
+++ Difficulty: Medium/Largest square formed in a matrix/largest-square-formed-in-a-matrix.cpp
@@ -10,30 +10,30 @@ using namespace std;
 
 class Solution {
   public:
-    int solve(vector<vector<int>>&mat,int i,int j,int &maxi,vector<vector<int>>&dp){
-        if(i>=mat.size() || j>=mat[0].size()){
-            return 0;
+    int solveTab(vector<vector<int>> mat,int &maxi){
+        int row=mat.size();
+        int col=mat[0].size();
+        vector<vector<int>>dp(row+1,vector<int>(col+1,0));
+        for(int i=row-1;i>=0;i--){
+            for(int j=col-1;j>=0;j--){
+                int right=dp[i][j+1];
+                int digonal=dp[i+1][j+1];
+                int down=dp[i+1][j];
+                if(mat[i][j]==1){
+                    dp[i][j]=1+min({right,digonal,down});
+                    maxi=max(maxi,dp[i][j]);
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int right=solve(mat,i,j+1,maxi,dp);
-        int digonal=solve(mat,i+1,j+1,maxi,dp);
-        int down=solve(mat,i+1,j,maxi,dp);
-        if(mat[i][j]==1){
-            int ans=1+min({right,digonal,down});
-            maxi=max(maxi,ans);
-            return dp[i][j]=ans;
-        }
-        else{
-            return dp[i][j]=0;
-        }
+        return dp[0][0];
     }
     int maxSquare(int n, int m, vector<vector<int>> mat) {
         // code here
         int maxi=0;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        solve(mat,0,0,maxi,dp);
+        solveTab(mat,maxi);
         return maxi;
     }
 };
