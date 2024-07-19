d405bf8f1a4309203af5617c0f23b6446c3f613c
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-19 11:05:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    string solve(string word){
+        vector<int>v(26,0);
+        for(int i=0;i<word.length();i++){
+            v[word[i]-'a']++;
+        }
+        string temp="";
+        for(int i=0;i<26;i++){
+            int freq=v[i];
+            temp+=string(freq,i+'a');
+        }
+        return temp;
+    }
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(int i=0;i<string_list.size();i++){
+            string word=string_list[i];
+            string newWord=solve(word);
+            mp[newWord].push_back(word);
+        }
+        for(auto i:mp){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
