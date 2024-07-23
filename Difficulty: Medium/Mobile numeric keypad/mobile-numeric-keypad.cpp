017d4b4ba72c67cbeb845c996d17a924ed94fdbc
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-07-23 15:55:47 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mobile numeric keypad/mobile-numeric-keypad.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+     vector<vector<int>> adj{{0,8},{1,2,4},{2,1,3,5},{3,2,6},{4,1,5,7},{5,2,4,6,8},{6,3,5,9},{7,4,8},{8,0,5,7,9},{9,6,8}};
+     long long solve(int n,int cell,vector<vector<long long>>&dp){
+        if(n==1){
+            return 1;
+        }
+          
+        if(dp[n][cell] != -1){
+            return dp[n][cell];
+        }
+        long long ans=0;
+        for(int &next:adj[cell]){
+            ans=(ans+solve(n-1,next,dp));
+        }
+        return dp[n][cell]=ans;
+     }
+    long long getCount(int n) {
+        vector <vector<long long>> dp(n+1,vector <long long>(10,-1));
+        long long res=0;
+        for(int i =0 ; i < 10 ; i++){
+            long long h = solve(n,i,dp);
+            res+=h;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        cout << ob.getCount(n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
