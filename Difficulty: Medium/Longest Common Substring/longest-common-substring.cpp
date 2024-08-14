168cea8839f5cbe4ae41ea72892582c172542177
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-08-14 13:50:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,53 +1,50 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution{
-    public:
+class Solution {
+  public:
     int m,n;
     int ans=0;
-    int solve(string &s1,string& s2,int i,int j,vector<vector<int>>&dp){
+    int solve(string &str1,string &str2,int i,int j,vector<vector<int>>&dp){
         if(i>=m || j>=n){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int cnt=0;
-        if(s1[i]==s2[j]){
-            cnt=1+solve(s1,s2,i+1,j+1,dp);
-            ans=max(ans,cnt);
+        int count=0;
+        if(str1[i]==str2[j]){
+            count=1+solve(str1,str2,i+1,j+1,dp);
+            ans=max(ans,count);
         }
-        int moveS1=solve(s1,s2,i+1,j,dp);
-        int moveS2=solve(s1,s2,i,j+1,dp);
-        return dp[i][j]=cnt;
+        int move1=solve(str1,str2,i+1,j,dp);
+        int move2=solve(str1,str2,i,j+1,dp);
+        return dp[i][j]=count;
     }
-    int longestCommonSubstr (string S1, string S2, int M, int N)
-    {
+    int longestCommonSubstr(string str1, string str2) {
         // your code here
-        m=M;n=N;
+        m=str1.length();
+        n=str2.length();
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        solve(S1,S2,0,0,dp);
+        solve(str1,str2,0,0,dp);
         return ans;
     }
 };
 
 //{ Driver Code Starts.
 
-int main()
-{
-    int t; cin >> t;
-    while (t--)
-    {
-        int n, m; cin >> n >> m;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
         string s1, s2;
         cin >> s1 >> s2;
         Solution ob;
 
-        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+        cout << ob.longestCommonSubstr(s1, s2) << endl;
     }
 }
-// Contributed By: Pranay Bansal
-
 // } Driver Code Ends
