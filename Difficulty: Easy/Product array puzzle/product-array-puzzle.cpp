d6c5d2c70a43a7d1a6bad8fb610ddd54ae66fe18
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-08-17 20:36:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,61 +1,66 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-class Solution{
+class Solution {
   public:
     // nums: given vector
     // return the Product vector P that hold product except self at each index
-    vector<long long int> productExceptSelf(vector<long long int>& nums, int n) {
-       
-        //code here        
-        vector<long long int>output(n);
-        long long int prod=1;
-        for(int i=0;i<n;i++){
-            output[i]=prod;
-            prod*=nums[i];
+    vector<long long int> productExceptSelf(vector<long long int>& nums) {
+        long long prod=1;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                prod*=nums[i];
+            }
+            else count++;
         }
-        prod=1;
-        for(int i=n-1;i>=0;i--){
-            output[i]*=prod;
-            prod*=nums[i];
+        vector<long long int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(count==1){
+                if(nums[i]==0){
+                ans.push_back(prod);
+                }
+                else{
+                    ans.push_back(0);
+                }
+            }
+            else if(count>=2) ans.push_back(0);
+            else ans.push_back(prod/nums[i]);
         }
-        return output;
+        return ans;
     }
 };
 
 
 //{ Driver Code Starts.
-int main()
- {
-    int t;  // number of test cases
-    cin>>t;
-    while(t--)
-    {
-        int n;  // size of the array
-        cin>>n;
-        vector<long long int> arr(n),vec(n);
-        
-        for(int i=0;i<n;i++)    // input the array
+int main() {
+    int t; // number of test cases
+    cin >> t;
+    while (t--) {
+        int n; // size of the array
+        cin >> n;
+        vector<long long int> arr(n), vec(n);
+
+        for (int i = 0; i < n; i++) // input the array
         {
-            cin>>arr[i];
+            cin >> arr[i];
         }
         Solution obj;
-        vec = obj.productExceptSelf(arr,n);   // function call
-        
-        for(int i=0;i<n;i++)    // print the output
+        vec = obj.productExceptSelf(arr); // function call
+
+        for (int i = 0; i < n; i++) // print the output
         {
             cout << vec[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-	return 0;
+    return 0;
 }
 // } Driver Code Ends
