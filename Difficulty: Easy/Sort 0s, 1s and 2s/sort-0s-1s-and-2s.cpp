23e648bb5619f6aabec48eae138f7cbe69da6d10
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-09-09 15:58:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int i=0,j=n-1,k=0;
+        while(k<=j){
+            if(arr[k]==0){
+                swap(arr[i],arr[k]);
+                k++;
+                i++;
+            }
+            else if(arr[k]==1){
+                k++;
+            }
+            else{
+                swap(arr[j],arr[k]);
+                j--;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
