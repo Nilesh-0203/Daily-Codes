be4332aabddd59403194f360a3a4c6c6331f6565
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-10-06 11:46:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -6,11 +6,11 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
+    vector<vector<int>>directions{{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
     int m,n;
-    vector<vector<int>>directions{{-1,0},{1,0},{0,1},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
-    void dfs(vector<vector<char>>&grid,int i,int j){
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0'){
-            return ;
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0'){
+            return;
         }
         grid[i][j]='0';
         for(auto dir:directions){
@@ -19,17 +19,15 @@ class Solution {
             dfs(grid,new_i,new_j);
         }
     }
-    
     int numIslands(vector<vector<char>>& grid) {
         // Code here
-        m=grid.size();
-        n=grid[0].size();
+        m=grid.size(),n=grid[0].size();
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
-                    count++;
                     dfs(grid,i,j);
+                    count++;
                 }
             }
         }
