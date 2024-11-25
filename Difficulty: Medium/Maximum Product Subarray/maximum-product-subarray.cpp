339f06cb2a2ea7837b44ad607cfcc963a447682f
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-11-25 09:51:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -3,18 +3,19 @@
 
 using namespace std;
 
-// } Driver Code Ends
-//User function template for C++
-class Solution{
-public:
 
-	// Function to find maximum product subarray
-	long long maxProduct(vector<int> arr, int n) {
-	    // code here
-	    long long  prod=1;
-	    long long  largest=LLONG_MIN;
-	    for(int i=0;i<n;i++){
-	        if(prod==1000000000){
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int prod=1;
+        int largest=INT_MIN;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(prod==1000000000){
                 break;
             }
             prod*=arr[i];
@@ -22,10 +23,10 @@ public:
             if(prod==0){
                 prod=1;
             }
-	    }
-	    prod=1;
-	    for(int i=n-1;i>=0;i--){
-	        if(prod==1000000000){
+        }
+        prod=1;
+        for(int i=n-1;i>=0;i--){
+            if(prod==1000000000){
                 break;
             }
             prod*=arr[i];
@@ -33,9 +34,9 @@ public:
             if(prod==0){
                 prod=1;
             }
-	    }
-	    return largest;
-	}
+        }
+        return largest;
+    }
 };
 
 //{ Driver Code Starts.
@@ -43,16 +44,31 @@ public:
 int main() {
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n, i;
-        cin >> n;
-        vector<int> arr(n);
-        for (i = 0; i < n; i++) {
-            cin >> arr[i];
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
         Solution ob;
-        auto ans = ob.maxProduct(arr, n);
+        auto ans = ob.maxProduct(arr);
         cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
