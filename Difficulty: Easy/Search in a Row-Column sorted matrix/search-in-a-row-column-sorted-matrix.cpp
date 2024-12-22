41268919ab8a82deb000b7057e45335dc839bf5e
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2024-12-22 11:10:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Row-Column sorted matrix/search-in-a-row-column-sorted-matrix.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool matSearch(vector<vector<int>> &mat, int x) {
+        // your code here
+        int m=mat.size();
+        int n=mat[0].size();
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==x)
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        int x;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        cin >> x;
+        Solution ob;
+        bool an = ob.matSearch(matrix, x);
+        if (an)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
