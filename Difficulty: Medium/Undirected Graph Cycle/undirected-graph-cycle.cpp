f957af462c5dbe9e00b30ccc22144c6d39474efc
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-04-04 18:19:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -2,11 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
+
 class Solution {
   public:
-    // Function to detect cycle in an undirected graph.
-    bool isCycledfs(vector<int> adj[],unordered_map<int,bool>&visited,int node,int parent){
+    bool isCycledfs(unordered_map<int,list<int>>&adj,unordered_map<int,bool>&visited,int node,int parent){
         visited[node]=true;
         for(auto neighbor:adj[node]){
             if(!visited[neighbor]){
@@ -21,8 +23,17 @@ class Solution {
         }
         return false;
     }
-    bool isCycle(int V, vector<int> adj[]) {
+    bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
+        unordered_map<int,list<int>>adj;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
         unordered_map<int,bool>visited;
         for(int i=0;i<V;i++){
             if(!visited[i]){
@@ -36,27 +47,37 @@ class Solution {
     }
 };
 
+
+
+
 //{ Driver Code Starts.
+
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
         int V, E;
         cin >> V >> E;
-        vector<int> adj[V];
-        for (int i = 0; i < E; i++) {
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            edges.push_back({u, v});
         }
+
         Solution obj;
-        bool ans = obj.isCycle(V, adj);
+        bool ans = obj.isCycle(V, edges);
         if (ans)
-            cout << "1\n";
+            cout << "true\n";
         else
-            cout << "0\n";
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
+
 // } Driver Code Ends
