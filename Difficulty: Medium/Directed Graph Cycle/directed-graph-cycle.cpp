638a3ccd7ca34228311cc8b0936e47781d96e2f9
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-04-07 19:24:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -2,63 +2,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
+
+
 class Solution {
   public:
-    // Function to detect cycle in a directed graph.
-    bool isCyclic(int V, vector<int> adj[]) {
+    bool dfs(unordered_map<int,list<int>>&adj,unordered_map<int,bool>&visited,unordered_map<int,bool>&dfsVisited,int node){
+        visited[node]=true;
+        dfsVisited[node]=true;
+        for(auto neighbor:adj[node]){
+            if(!visited[neighbor]){
+                bool cycle=dfs(adj,visited,dfsVisited,neighbor);
+                if(cycle){
+                    return true;
+                }
+            }
+            else if(dfsVisited[neighbor]==true){
+                return true;
+            }
+        }
+        dfsVisited[node]=false;
+        return false;
+    }
+    bool isCyclic(int V, vector<vector<int>> &edges) {
         // code here
-        int count=0;
-        vector<int>inorder(V);
-        for(auto i=0;i<V;i++){
-            for(auto j:adj[i]){
-                inorder[j]++;
-            }
+        unordered_map<int,bool>visited;
+        unordered_map<int,bool>dfsVisited;
+        unordered_map<int,list<int>>adj;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+            adj[u].push_back(v);
         }
-        queue<int>q;
+        
         for(int i=0;i<V;i++){
-            if(inorder[i]==0){
-                q.push(i);
-            }
-        }
-        while(!q.empty()){
-            int front=q.front();
-            q.pop();
-            count++;
-            for(auto neighbor:adj[front]){
-                inorder[neighbor]--;
-                if(inorder[neighbor]==0){
-                    q.push(neighbor);
+            if(!visited[i]){
+                bool cycle=dfs(adj,visited,dfsVisited,i);
+                if(cycle){
+                    return true;
                 }
             }
         }
-        return count!=V;
+        return false;
     }
 };
+
+
+
 
 //{ Driver Code Starts.
 
 int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+    while (tc--) {
         int V, E;
         cin >> V >> E;
-
-        vector<int> adj[V];
-
-        for (int i = 0; i < E; i++) {
+        cin.ignore();
+        vector<vector<int>> edges;
+        for (int i = 1; i <= E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
+            edges.push_back({u, v});
         }
 
         Solution obj;
-        cout << obj.isCyclic(V, adj) << "\n";
+        bool ans = obj.isCyclic(V, edges);
+        if (ans)
+            cout << "true\n";
+        else
+            cout << "false\n";
     }
-
     return 0;
 }
-
 // } Driver Code Ends
