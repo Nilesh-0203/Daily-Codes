84c3f67dc3e8f57c116927d8b408b46dde43906f
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-04-16 12:12:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.cpp
@@ -1,63 +1,57 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-//User function template for C++
+
+// User function template for C++
 
 class Solution {
   public:
-	void shortest_distance(vector<vector<int>>&matrix){
-	    // Code here
-	    int n=matrix.size();
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n;j++){
-	            if(matrix[i][j]==-1){
-	                matrix[i][j]=100000;
-	            }
-	        }
-	    }
-	    for(int via=0;via<n;via++){
-	        for(int i=0;i<n;i++){
-    	        for(int j=0;j<n;j++){
-    	           matrix[i][j]=min(matrix[i][j],matrix[i][via]+matrix[via][j]);
-    	        }
-    	    }
-	    }
-	    for(int i=0;i<n;i++){
-	        for(int j=0;j<n;j++){
-	            if(matrix[i][j]==100000){
-	                matrix[i][j]=-1;
-	            }
-	        }
-	    }
-	}
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n=dist.size();
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][via]!=1e8 && dist[via][j]!=1e8){
+                        dist[i][j]=min(dist[i][j],dist[i][via]+dist[via][j]);
+                    }
+                }
+            }
+        }
+    }
 };
 
+
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<vector<int>>matrix(n, vector<int>(n, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cin >> matrix[i][j];
-			}
-		}
-		Solution obj;
-		obj.shortest_distance(matrix);
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-				cout << matrix[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.floydWarshall(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
 // } Driver Code Ends
