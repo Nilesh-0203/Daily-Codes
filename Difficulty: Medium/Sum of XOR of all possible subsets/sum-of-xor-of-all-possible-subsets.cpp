54054f59910681b34c7aea822808c3c5118fba23
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-04-24 15:12:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of XOR of all possible subsets/sum-of-xor-of-all-possible-subsets.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+class Solution {
+  public:
+    int subsetXORSum(vector<int>& arr) {
+        // code here
+        int result=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            result|=arr[i];
+        }
+        return result<<(n-1);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.subsetXORSum(a) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
