c5140c6b8a515ab752ac1dbd797e0d3f702f1982
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-05-09 14:46:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
+++ Difficulty: Medium/Largest number in K swaps/largest-number-in-k-swaps.cpp
@@ -6,57 +6,51 @@ using namespace std;
 // } Driver Code Ends
 
 
-class Solution
-{
-    public:
-    //Function to find the largest number after k swaps.
-    void solve(string &max,string str,int k,int idx){
-        if(k==0){
+
+class Solution {
+  public:
+    // Function to find the largest number after k swaps.
+    void solve(int idx,string&s,string&ans,int k){
+        if(k==0 || idx>=s.length()){
+            ans=max(ans,s);
             return;
         }
-        int n=str.length();
-        char maxc=str[idx];
-        for(int i=idx+1;i<n;i++){
-            if(maxc<str[i]){
-                maxc=str[i];
+        char mx=s[idx];
+        for(int i=idx;i<s.length();i++){
+            if(i==idx){
+                solve(idx+1,s,ans,k);
             }
-        }
-        if(maxc!=str[idx]){
-            k-=1;
-        }
-        for(int i=idx;i<n;i++){
-            if(str[i]==maxc){
-                swap(str[i],str[idx]);
-                if(str.compare(max)>0){
-                    max=str;
-                }
-                solve(max,str,k,idx+1);
-                swap(str[i],str[idx]);
+            if(s[i]>mx){
+                swap(s[i],s[idx]);
+                solve(idx+1,s,ans,k-1);
+                swap(s[i],s[idx]);
             }
         }
     }
-    string findMaximumNum(string str, int k)
-    {
-       // code here.
-       string max=str;
-       solve(max,str,k,0);
-       return max;
+    string findMaximumNum(string& s, int k) {
+        // code here.
+        string ans;
+        int idx=0;
+        solve(idx,s,ans,k);
+        return ans;
     }
 };
 
+
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t, k;
     string str;
 
     cin >> t;
-    while (t--)
-    {
+    while (t--) {
         cin >> k >> str;
         Solution ob;
-        cout<< ob.findMaximumNum(str, k) << endl;
+        cout << ob.findMaximumNum(str, k) << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
