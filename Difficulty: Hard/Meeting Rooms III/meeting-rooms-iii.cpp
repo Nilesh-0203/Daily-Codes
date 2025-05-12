9b44a9cdbcc5dd9ba660d2c59ebc459d8566c7ea
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-05-12 20:12:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Meeting Rooms III/meeting-rooms-iii.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+
+bool compare(vector<int>& v1, vector<int>& v2) {
+    return v1[0] < v2[0];
+}
+
+class Solution {
+  public:
+    int mostBooked(int n, vector<vector<int>> &meetings) {
+        // code here
+        sort(meetings.begin(), meetings.end(), compare);
+        
+        /* Create a priority queue for engaged rooms. Each node of PQ will store {current_meeting_ending_time, room_number} */
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> engaged;
+        
+        /* Create a priority queue for non-engaged rooms. Each node of PQ will store {room_number} */
+        priority_queue<int, vector<int>, greater<int>> unused;
+        
+        /* Frequency map to store the frequency of meetings in each room */
+        unordered_map<int, int> freq;
+        
+        /* Currently all the rooms are mepty */
+        for(int i = 0; i < n; i++) {
+            unused.push(i);
+        }
+        
+        for(auto x:meetings) {
+            int s = x[0], e = x[1];
+            
+            /* Move the meeting rooms in engaged, with ending_time <= s, to unused */ 
+            while(engaged.size() > 0 && engaged.top().first <= s) {
+                int room = engaged.top().second;
+                engaged.pop();
+                
+                unused.push(room);
+            }
+            
+            /* If there are multiple empty rooms, choose the one with lower room_number */
+            if(unused.size() > 0) 
+            {
+                int room = unused.top();
+                unused.pop();
+                
+                freq[abs(room)] += 1;
+                
+                /* Mark the room as engaged */
+                engaged.push({e, room});
+            }
+            /* If there are no empty rooms, wait for the engaged room with nearest ending time to empty */
+            else 
+            {
+                pair<long long, int> topmost = engaged.top();
+                engaged.pop();
+            
+                freq[abs(topmost.second)] += 1;
+                
+                /* Since duration has to be the same, the newEnd will be sum(end_time_of_the_prev_meeting, duration) */ 
+                long long newEnd = topmost.first;
+                newEnd += (e - s);
+                
+                /* Mark the room as engaged */
+                engaged.push({newEnd, topmost.second});
+            }
+        }
+        
+        /* Find the lowest room_number with maximum frequency */
+        int maxi = 0;
+        for(int i = 1; i < n; i++) {
+            if(freq[i] > freq[maxi])
+                maxi = i;
+        }
+        return maxi;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        cin >> m;
+        vector<vector<int>> meetings(m, vector<int>(2));
+        for (int i = 0; i < m; i++) {
+            cin >> meetings[i][0] >> meetings[i][1];
+        }
+        Solution ob;
+        cout << ob.mostBooked(n, meetings) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
