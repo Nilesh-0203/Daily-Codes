d0bb5db31049fbe265cb209473c7dc03f0be8ec0
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-06-20 00:10:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Case-specific Sorting of Strings/casespecific-sorting-of-strings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+     string caseSort(string& s) {
+        // code here
+        vector<int>u(26,0);
+        vector<int>l(26,0);
+        for( char c : s ){
+            if( c >= 'A' and c <= 'Z' ){
+                u[c-'A']++;
+            }
+            if( c >= 'a' and c <= 'z' ){
+                l[c-'a']++;
+            }
+        }
+        string U = "";
+        for( int i = 0 ; i < 26 ; i++ ){
+            while(u[i]>0){
+                U += ('A' + i);
+                u[i]--;
+            }
+        }
+        string L = "";
+        for( int i = 0 ; i < 26 ; i++ ){
+            while(l[i]>0){
+                L += ('a' + i);
+                l[i]--;
+            }
+        }
+        int ui = 0 , li = 0;
+        for( int i = 0 ; i < s.length() ; i++ ){
+            char c = s[i];
+            if( c >= 'A' and c <= 'Z' ){
+                s[i] = U[ui];
+                ui++;
+            }
+            if( c >= 'a' and c <= 'z' ){
+                s[i] = L[li];
+                li++;
+            }
+        }
+        return s;
+    }
+};
