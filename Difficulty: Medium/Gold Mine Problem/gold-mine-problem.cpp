f088db52286a76c005df234f3f50596abf31b172
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-07-12 17:05:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gold Mine Problem/gold-mine-problem.cpp
+++ Difficulty: Medium/Gold Mine Problem/gold-mine-problem.cpp
@@ -1,58 +1,27 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution{
-public:
-    int N,M;
-    int solve(vector<vector<int>>&mine,int i,int j,vector<vector<int>>&dp){
-        if(i<0 || j<0 || i>=N || j>=M){
+class Solution {
+  public:
+    int n,m;
+    int solve(vector<vector<int>>&mat,int i,int j,vector<vector<int>>&dp){
+        if(i<0 || j<0 || i>=m || j>=n){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int right=mine[i][j]+solve(mine,i,j+1,dp);
-        int rightUp=mine[i][j]+solve(mine,i-1,j+1,dp);
-        int rightDown=mine[i][j]+solve(mine,i+1,j+1,dp);
-    
-        return dp[i][j]=max(right,max(rightUp,rightDown));
+        int rightUp=mat[i][j]+solve(mat,i-1,j+1,dp);
+        int right=mat[i][j]+solve(mat,i,j+1,dp);
+        int rightDown=mat[i][j]+solve(mat,i+1,j+1,dp);
+        
+        return dp[i][j]=max(rightUp,max(right,rightDown));
     }
-    int maxGold(int n, int m, vector<vector<int>> mine)
-    {
+    int maxGold(vector<vector<int>>& mat) {
         // code here
+        m=mat.size(),n=mat[0].size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         int ans=0;
-        N=n,M=m;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<n;i++){
-            ans=max(ans,solve(mine,i,0,dp));
+        for(int i=0;i<m;i++){
+            ans=max(ans,solve(mat,i,0,dp));
         }
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n, m;
-        cin>>n>>m;
-        vector<vector<int>> M(n, vector<int>(m, 0));
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++)
-                cin>>M[i][j];
-        }
-        
-        Solution ob;
-        cout<<ob.maxGold(n, m, M)<<"\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
