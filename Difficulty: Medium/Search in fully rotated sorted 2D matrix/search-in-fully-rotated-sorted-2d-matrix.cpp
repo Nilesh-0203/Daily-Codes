bb7463f364fcffa397305a1c3481bd4f6b267d13
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-08-21 03:35:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &mat, int x) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        bool flag = false;
+        vector<int> rows;
+        for (int i = 0; i < n; i++) {
+            sort(mat[i].begin(),mat[i].end()); //(nlogn)
+            if (mat[i][0] <= x && mat[i][m - 1] >= x) {
+                flag = true;
+                rows.push_back(i);
+            }
+        }
+        if (!flag) return false;
+        for (int r : rows) { //[0,1]
+            int start = 0, end = m - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (mat[r][mid] == x) return true;
+                else if (mat[r][mid] > x) end = mid - 1;
+                else start = mid + 1;
+            }
+        }
+        return false;
+    }
+
+};
