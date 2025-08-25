718a826ee8b6e10ce2ec10893019908429a0a39c
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-08-25 19:05:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximize median after doing k addition operation/maximize-median-after-doing-k-addition-operation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    int maximizeMedian(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        int index = -1;
+        if (n % 2 == 0){
+            // even
+            index = (n / 2) - 1;
+        }
+        else{
+            index = n / 2;
+        }
+        
+        sort(arr.begin(), arr.end());
+        int count = 0;
+        for (int i = index; i < n - 1; i++){
+            if (arr[i] < arr[i + 1]){
+                int index_diff = i - index + 1;
+                int diff = (arr[i + 1] - arr[i]) * index_diff;
+                if (diff >= k){
+                    if (count == 0 && n % 2 == 0){
+                        return ((arr[i] + (k / index_diff)) + arr[i + 1]) / 2;
+                    }
+                    return arr[i] + (k / index_diff);
+                }
+                else{
+                    // convert all elements to arr[i + 1]
+                    k = k - diff;
+                }
+            }
+            count++;
+        }
+        
+        // if (k > 0){
+            return arr[n - 1] + (k / (n - index));
+        // }
+        
+    }
+};
