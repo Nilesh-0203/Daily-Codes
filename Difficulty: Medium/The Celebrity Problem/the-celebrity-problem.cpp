05f5c0888380c723102dcdd978f89e38ce9595fe
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-08-30 16:26:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,20 +1,7 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    int celebrity(vector<vector<int> >& M) 
-    
-    {
-        // code here 
+    int celebrity(vector<vector<int>>& M) {
+        // code here
         int n=M.size();
         stack<int>st;
         for(int i=0;i<n;i++){
@@ -44,26 +31,3 @@ class Solution {
         return candi;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
