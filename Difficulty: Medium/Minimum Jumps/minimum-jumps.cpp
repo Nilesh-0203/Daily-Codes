19d0b346020a0e768f7eaaf7a6be0476aa7b48c9
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-09-11 17:39:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,61 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int n;
-    int solve(vector<int>&arr,int idx,vector<int>&dp){
-        if(idx>=(n-1)){
-            return 0;
-        }
-        if(arr[idx]==0){
-            return 1e7;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int ans=1e7;
-        for(int i=idx+1;i<=idx+arr[idx];i++){
-            ans=min(ans,1+solve(arr,i,dp));
-        }
-        return dp[idx]=ans;
-    }
     int minJumps(vector<int>& arr) {
         // code here
-        n=arr.size();
-        vector<int>dp(n+1,-1);
-        int res=solve(arr,0,dp);
-        return res==1e7 ? -1 : res;
-    }
-};
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
+        int jump = 0;
+        int maxJump = 0;
+        int currMax = 0;
+        int n = arr.size();
+        for(int i=0;i<n-1;i++){
+            maxJump = max(maxJump,i+arr[i]);
+            if(currMax == i){
+                currMax = max(currMax,maxJump);
+                jump++;
+            }
         }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
+        if(currMax >= n-1) return jump;
+        return -1;
     }
-    return 0;
-}
-
-// } Driver Code Ends
+ 
+};
