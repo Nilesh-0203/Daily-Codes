932150603796ea5726d2fe56ba53c1635225890e
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-09-12 18:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.cpp
@@ -1,16 +1,7 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    int getMinDiff(vector<int>& arr, int k) {
+    int getMinDiff(vector<int> &arr, int k) {
+        // code here
         int n = arr.size();
         if (n == 1) return 0;  // If there's only one tower, difference is 0.
         
@@ -35,33 +26,6 @@ class Solution {
             // Update the result with the minimum possible difference
             result = min(result, maxHeight - minHeight);
         }
-
         return result;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, k;
-        cin >> k;
-        cin.ignore();
-        vector<int> a, b, c, d;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution ob;
-        auto ans = ob.getMinDiff(a, k);
-        cout << ans << "\n";
-        cout << '~' << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
