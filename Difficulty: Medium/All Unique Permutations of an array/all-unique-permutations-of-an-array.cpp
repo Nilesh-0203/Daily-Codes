0583f4a3186866acff994efaf01edd0737306785
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-10-01 19:44:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.cpp
+++ Difficulty: Medium/All Unique Permutations of an array/all-unique-permutations-of-an-array.cpp
@@ -1,13 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 class Solution {
   public:
     void solve(vector<int>arr,int idx,set<vector<int>>&st){
@@ -21,8 +11,9 @@ class Solution {
             swap(arr[idx],arr[i]);
         }
     }
-    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+    vector<vector<int>> uniquePerms(vector<int>& arr) {
         // code here
+        int n=arr.size();
         set<vector<int>>st;
         solve(arr,0,st);
         vector<vector<int>>v;
@@ -32,32 +23,3 @@ class Solution {
         return v;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        
-        cin>>n;
-        vector<int> arr(n);
-        
-        for(int i=0 ; i<n ; i++)
-            cin>>arr[i];
-
-        Solution ob;
-        vector<vector<int>> res = ob.uniquePerms(arr,n);
-        for(int i=0; i<res.size(); i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                cout<<res[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-    }
-    return 0;
-}
-// } Driver Code Ends
