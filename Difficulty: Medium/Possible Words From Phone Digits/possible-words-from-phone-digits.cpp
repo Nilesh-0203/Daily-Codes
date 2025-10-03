61ef1f1bc97be20eab8c4b01b39a85e6933953bd
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-10-03 21:32:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.cpp
+++ Difficulty: Medium/Possible Words From Phone Digits/possible-words-from-phone-digits.cpp
@@ -1,77 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-class Array {
-  public:
-    template <class T>
-    static void input(vector<T> &A, int n) {
-        for (int i = 0; i < n; i++) {
-            scanf("%d ", &A[i]);
-        }
-    }
-
-    template <class T>
-    static void print(vector<T> &A) {
-        for (int i = 0; i < A.size(); i++) {
-            cout << A[i] << " ";
-        }
-        cout << endl;
-    }
-};
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
-    void solve(string mp[10],vector<string>&ans,string output,int idx,vector<int>&arr){
-        if(idx>=arr.size()){
-            ans.push_back(output);
+  void helper(int i, int n, vector<string> &mp, vector<int> &arr, string &tmp, 
+    vector<string> &ans) {
+        if(i == n) {
+            ans.push_back(tmp);
             return;
         }
-        string temp=mp[arr[idx]];
-        for(int i=0;i<temp.length();i++){
-            output.push_back(temp[i]);
-            solve(mp,ans,output,idx+1,arr);
-            output.pop_back();
+        if(mp[arr[i]] == "") {
+            helper(i + 1, n, mp, arr, tmp, ans);
+            return;
+        }
+        for(int k = 0; k < mp[arr[i]].length(); k++) {
+            tmp += mp[arr[i]][k];
+            helper(i + 1, n, mp, arr, tmp, ans);
+            tmp = tmp.substr(0, tmp.size() - 1);
         }
     }
+  public:
     vector<string> possibleWords(vector<int> &arr) {
         // code here
-        vector<string>ans;
-        if(arr.size()==0){
-            return ans;
-        }
-        string output="";
-        string mp[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(mp,ans,output,0,arr);
+        int n = arr.size();
+        vector<string> mp(10), ans;
+        mp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        string tmp = "";
+        helper(0, n, mp, arr, tmp, ans);
         return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution obj;
-        vector<string> v = obj.possibleWords(a);
-        sort(v.begin(), v.end());
-        Array::print(v);
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
