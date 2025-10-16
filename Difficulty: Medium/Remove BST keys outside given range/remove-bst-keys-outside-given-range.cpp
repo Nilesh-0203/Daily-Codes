540a6354afb5601a9d2a41f16173c4826bf70536
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-10-16 22:25:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove BST keys outside given range/remove-bst-keys-outside-given-range.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  Node* solve(Node*root,int l,int r){
+        if(!root) return NULL;
+        
+        if(root->data >= l && root->data <= r){
+            root->left = solve(root->left,l,r);
+            root->right = solve(root->right,l,r);
+            return root;
+        }
+        else if(root->data > r ){
+            return solve(root->left,l,r);
+        }else return solve(root->right,l,r);
+    }
+  public:
+    Node* removekeys(Node* root, int l, int r) {
+        // code here
+        return solve(root,l,r);
+    }
+    
+};
