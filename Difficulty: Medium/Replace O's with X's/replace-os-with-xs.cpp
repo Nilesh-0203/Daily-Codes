d1af3f885d3a26a035cf21d1a0b75095ef3e0a8c
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-10-30 14:18:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Difficulty: Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -1,86 +1,65 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
-class Solution{
-public:
-    vector<vector<int>>directions{{1,0},{-1,0},{0,1},{0,-1}};
-    int m,n;
-    void dfs(vector<vector<char>>& grid,int i,int j){
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!='O'){
-            return;
-        }
-        grid[i][j]='$';
-        for(auto &dir:directions){
-            int new_i=i+dir[0];
-            int new_j=j+dir[1];
-            dfs(grid,new_i,new_j);
-        }
-    }
-    vector<vector<char>> fill(int M, int N, vector<vector<char>> board)
-    {
-        // code here
-        m=board.size();
-        n=board[0].size();
-        for(int i=0;i<m;i++){
-            if(board[i][0]=='O'){  //First Columne
-                dfs(board,i,0);
+class Solution {
+  public:
+    void fill(vector<vector<char>>& grid) {
+        // Code here
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        queue<pair<int, int>>q;
+        
+        for(int i=0; i<n; i++){
+            if(grid[i][0]=='O'){
+                grid[i][0]='C';
+                q.push({i, 0});
             }
-            if(board[i][n-1]=='O'){ //Last Columne
-                dfs(board,i,n-1);
+            if(grid[i][m-1]=='O'){
+                grid[i][m-1]='C';
+                q.push({i, m-1});
             }
         }
-
-        for(int i=0;i<n;i++){
-            if(board[0][i]=='O'){  //First Row
-                dfs(board,0,i);
+        
+        for(int j=0; j<m; j++){
+            if(grid[0][j]=='O'){
+                grid[0][j]='C';
+                q.push({0, j});
             }
-            if(board[m-1][i]=='O'){ //Last Row
-                dfs(board,m-1,i);
+            if(grid[n-1][j]=='O'){
+                grid[n-1][j]='C';
+                q.push({n-1, j});
             }
         }
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]=='O'){
-                   board[i][j]='X';
-                }
-                else if(board[i][j]=='$'){
-                    board[i][j]='O';
+        
+        int dr[4]={0, 1, 0, -1};
+        int dc[4]={1, 0, -1, 0};
+        
+        while(!q.empty()){
+            auto it=q.front();
+            int r=it.first;
+            int c=it.second;
+            q.pop();
+            
+            for(int i=0; i<4; i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='O'){
+                    grid[nr][nc]='C';
+                    q.push({nr, nc});
                 }
             }
         }
-        return board;
+        
+        for(int i=1; i<n-1; i++){
+            for(int j=1; j<m-1; j++){
+                if(grid[i][j]=='O') grid[i][j]='X';
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]=='C'){
+                    grid[i][j]='O';
+                }
+            }
+        }
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n, m;
-        cin>>n>>m;
-        vector<vector<char>> mat(n, vector<char>(m, '.'));
-        for(int i = 0;i < n;i++)
-            for(int j=0; j<m; j++)
-                cin>>mat[i][j];
-        
-        Solution ob;
-        vector<vector<char>> ans = ob.fill(n, m, mat);
-        for(int i = 0;i < n;i++) {
-            for(int j = 0;j < m;j++) {
-                cout<<ans[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-    }
-    return 0;
-}
-// } Driver Code Ends
