9f75414565b614d40c1c1d247d65cb17b8de3e40
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2025-12-03 00:35:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximise String Score/maximise-string-score.cpp
+++ Difficulty: Medium/Maximise String Score/maximise-string-score.cpp
@@ -1,6 +1,6 @@
 class Solution {
   public:
-     int maxScore(string &s, vector<vector<char>> &jumps) {
+    int maxScore(string &s, vector<vector<char>> &jumps) {
         int n = s.size();
         if (n <= 1) return 0;
 
