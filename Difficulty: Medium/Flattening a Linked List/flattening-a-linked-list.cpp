7a79649b010299cf860b7807e09d35411c665916
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-01-03 23:25:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,11 +1,9 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
+/*
+class Node {
+public:
     int data;
-    struct Node* next;
-    struct Node* bottom;
+    Node* next;
+    Node* bottom;
 
     Node(int x) {
         data = x;
@@ -13,48 +11,11 @@ struct Node {
         bottom = NULL;
     }
 };
-
-void printList(Node* head) {
-    while (head != NULL) {
-        printf("%d ", head->data);
-        head = head->bottom;
-    }
-    printf("\n");
-}
-
-Node* createLinkedList(vector<Node*>& v) {
-    Node* head = new Node(0);
-    Node* temp = head;
-    int n = v.size();
-    for (int i = 0; i < n; i++) {
-        temp->next = v[i];
-        temp = temp->next;
-    }
-    return head->next;
-}
-
-
-// } Driver Code Ends
-/* Node structure  used in the program
-
-struct Node{
-    int data;
-    struct Node * next;
-    struct Node * bottom;
-
-    Node(int x){
-        data = x;
-        next = NULL;
-        bottom = NULL;
-    }
-
-};
 */
 
 class Solution {
   public:
-    // Function which returns the  root of the flattened linked list.
-Node* merge(Node* l1,Node* l2){
+    Node* merge(Node* l1,Node* l2){
         if(l1==NULL){
             return l2;
         }
@@ -79,46 +40,5 @@ Node* merge(Node* l1,Node* l2){
         Node* result=flatten(root->next);
         return merge(root,result);
     }
+
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n;
-        cin >> n;
-        cin.ignore();
-
-        vector<Node*> v(n);
-
-        for (int i = 0; i < n; i++) {
-            string line;
-            getline(cin, line);
-            stringstream ss(line);
-
-            Node* head = new Node(0);
-            Node* temp = head;
-            int x;
-            while (ss >> x) {
-                Node* newNode = new Node(x);
-                temp->bottom = newNode;
-                temp = temp->bottom;
-            }
-            v[i] = head->bottom;
-        }
-
-        Solution ob;
-        Node* list = createLinkedList(v);
-        Node* head = ob.flatten(list);
-        printList(head);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
