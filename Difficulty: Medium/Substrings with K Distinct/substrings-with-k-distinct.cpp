bace73f2089a67136b9a1bd5f92156b4a553ff75
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-01-10 20:44:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.cpp
@@ -1,45 +1,33 @@
 class Solution {
   public:
-    int countSubstr(string& s, int k) {
-        // code here.
-        int n=s.size();
-        vector<int>mp(26,0);
-        vector<int>dp(26,-1);
-        int j=0;
-        int ans=0;
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            mp[s[i]-'a']++;
-            dp[s[i]-'a']=i;
-            if(mp[s[i]-'a']==1)
-            {
-                ++count;
+     int f(string &s,int k){
+        vector<int> c(26,0);
+        int i =0;
+        int j =0;
+        int cnt =0;
+        int ccnt =0;
+        int n =s.size();
+        while(j < n){
+            if(c[s[j]-'a']){
+                c[s[j]-'a']++;
+            }else{
+                c[s[j]-'a']++;
+                ccnt++;
             }
-            while(j<i && count>k )
-            {
-                --mp[s[j]-'a'];
-                if(mp[s[j]-'a']==0)
-                {
-                    --count;
+            while(i <= j && ccnt == k){
+                c[s[i]-'a']--;
+                if(c[s[i]-'a'] == 0){
+                    ccnt--;
                 }
-                j++;
+                i++;
+                cnt+=(n-j);
             }
-            if(count<k)
-            {
-                continue;
-            }
-            int l_limit=INT_MAX;
-            for(int l=0;l<26;l++)
-            {
-                
-                if(dp[l]>=j)
-                {
-                  l_limit=min(l_limit,dp[l]);  
-                }
-            }
-            ans+=(l_limit-j+1);
+            j++;
         }
-        return ans;
+        return cnt;
+    }
+    int countSubstr(string& s, int k) {
+        // code here
+        return f(s,k)-f(s,k+1);
     }
 };
