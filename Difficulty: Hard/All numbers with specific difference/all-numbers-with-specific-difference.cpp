cca6d6fcf40ac3c58724585cc7c1836c93cba61c
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-02-13 12:44:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/All numbers with specific difference/all-numbers-with-specific-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int sumOfDigits(int x){
+            int sum = 0;
+            while(x > 0){
+                sum += x % 10;
+                x /= 10;
+            }
+            return sum;
+        }
+    int getCount(int n, int d) {
+        int low = 1, high = n,ans = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(mid - sumOfDigits(mid) >= d){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans != -1 ? n - ans + 1 : 0;
+    }
+};
