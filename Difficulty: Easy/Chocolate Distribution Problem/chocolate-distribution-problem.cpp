8b7d00a57aadb8c7a01adca28a9de4f07dcb71c1
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-02-15 19:51:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -1,42 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-class Solution{
-    public:
-    long long findMinDiff(vector<long long> a, long long n, long long m){
-    //code
-        sort(a.begin(),a.end());
-        long long ans=INT_MAX;
-        for(int i=0;i<=(n-m);i++){
-            ans=min(a[i+m-1]-a[i],ans);
+class Solution {
+  public:
+    int findMinDiff(vector<int>& a, int m) {
+        // code here
+         int n = a.size();
+        sort(a.begin() , a.end());
+        int j = 0;
+        int mini = INT_MAX;
+        for(int i=m-1;i<n;i++){
+            mini = min(mini , a[i] - a[j]);
+            j++;
         }
-        return ans;
-    }   
+        return mini;
+    }
 };
-
-//{ Driver Code Starts.
-int main() {
-	long long t;
-	cin>>t;
-	while(t--)
-	{
-		long long n;
-		cin>>n;
-		vector<long long> a;
-		long long x;
-		for(long long i=0;i<n;i++)
-		{
-			cin>>x;
-			a.push_back(x);
-		}
-		
-		long long m;
-		cin>>m;
-		Solution ob;
-		cout<<ob.findMinDiff(a,n,m)<<endl;
-	}
-	return 0;
-}
-// } Driver Code Ends
