a592525eafb0892b45d1f63f1b9023f3a772b6cf
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-03-20 20:53:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
+++ Difficulty: Medium/Predecessor and Successor/predecessor-and-successor.cpp
@@ -1,28 +1,5 @@
-//{ Driver Code Starts
-// C++ program to find predecessor and successor in a BST
-#include "bits/stdc++.h"
-using namespace std;
-
-// BST Node
-class Node {
-  public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int x) {
-        data = x;
-        left = NULL;
-        right = NULL;
-    }
-};
-
-
-// } Driver Code Ends
-
 /* BST Node
-class Node
-{
+class Node {
    public:
     int data;
     Node *left;
@@ -33,7 +10,8 @@ class Node
         left = NULL;
         right = NULL;
     }
-}; */
+};
+*/
 
 class Solution {
   public:
@@ -63,102 +41,3 @@ class Solution {
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-// Driver program to test above functions
-int main() {
-
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root = buildTree(s);
-        getline(cin, s);
-        int key = stoi(s);
-
-        Solution ob;
-        vector<Node*> result = ob.findPreSuc(root, key);
-        Node* pre = result[0];
-        Node* suc = result[1];
-
-        if (pre != nullptr)
-            cout << pre->data << " ";
-        else
-            cout << -1 << " ";
-
-        if (suc != nullptr)
-            cout << suc->data << endl;
-        else
-            cout << -1 << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
