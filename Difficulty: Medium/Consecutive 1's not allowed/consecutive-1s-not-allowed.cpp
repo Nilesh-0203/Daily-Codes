dc33d2f1212bb8c95b5aad4748bdca4d90aab041
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-04-01 22:19:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
+++ Difficulty: Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int countStrings(int n) {
         // code here
-         int a = 1;
+          int a = 1;
         int b = 1;
         for(int i =0;i<n;i++)
         {
