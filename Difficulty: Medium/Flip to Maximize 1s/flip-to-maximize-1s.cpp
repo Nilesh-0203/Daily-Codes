5176d9966b5af2b0a6f207b99a9ccfae5300aea6
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-04-18 23:11:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Flip to Maximize 1s/flip-to-maximize-1s.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+     int maxOnes(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int c =0 , s =0;
+         int n1=0;
+         for(int i =0;i<n;i++){
+             
+              if( arr[i]==0) c++;
+              else{
+                  c--;
+                   n1++;
+                  if( c<0) c=0;
+                  
+              }
+               s = max(s , c);
+         }
+          return n1+s;
+    }
+};
