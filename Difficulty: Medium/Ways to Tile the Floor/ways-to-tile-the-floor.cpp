3ebc04bb76b9d7e241bb59c86f6e090090b6d0cd
Nilesh Kudale <121235647+Nilesh-0203@users.noreply.github.com>
2026-06-27 21:02:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countWays(int n, int m) {
+        // code here.
+        vector<int>dp(n+m+1,0);
+        
+        const int mod = (int)(1e9)+7;
+        dp[n] = 1;
+        for(int i = n-1;i>=0;i--)
+        {
+            dp[i] = (dp[i+1]%mod + dp[i+m]%mod)%mod;
+        }
+        return dp[0];
+        
+    }
+};
